604f963e4161c7fef41b7d79d0751fa3ae0df7b9
Tomasz Stolarczyk <stolarczykt@gmail.com>
2015-01-10 17:53:30 +0100

    initial

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		less: {
+			development: {
+				options: {
+					paths: ["less/"]
+				},
+				files: {
+					"resources/css/default.css": "less/colors/default.less",
+					"resources/css/yellow-and-black.css": "less/colors/yellow-and-black.less"
+
+				}
+			}
+		},
+		watch: {
+			options: {
+				livereload: true
+			},
+			less: {
+				files: ['less/*.less', 'less/colors/*.less'],
+				tasks: ['less:development']
+			},
+			js: {
+				files: ['resources/js/*.js']
+			},
+			html: {
+				files: ['*.html']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+};
