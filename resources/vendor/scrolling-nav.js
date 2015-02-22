874aa84ec73c9709031c2f07cf2ad94ddef48fab
Tomasz Stolarczyk <stolarczykt@gmail.com>
2015-02-22 19:09:11 +0100

    navbar styling

--- resources/vendor/scrolling-nav.js
+++ resources/vendor/scrolling-nav.js
@@ -12,7 +12,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 80
+            scrollTop: $($anchor.attr('href')).offset().top - 70
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
